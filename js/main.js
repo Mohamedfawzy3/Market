570ab1212331871fd6c4c700b523fda16c5e5d56
Mohamedfawzy3 <mohammedromail94@gmail.com>
2024-12-23 11:40:07 +0200

    structure

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// =>>>>>>>>>>slider
+let sliderImages = [
+  "./images/slider1.jpg",
+  "./images/slider2.jpg",
+  "./images/slider3.jpg",
+  "./images/slider4.png",
+];
+let nextSliderBtn = document.querySelector(".slider .next");
+let previousSliderBtn = document.querySelector(".slider .previous");
+let sliderImg = document.querySelector(".slider img");
+// control slides
+sliderImg.src = sliderImages[0];
+let index = 0;
+let sliderIntervel=setInterval(()=>{
+   getNext()
+},5000)
+nextSliderBtn.onclick = () => {
+  getNext();
+};
+previousSliderBtn.onclick = () => {
+  getPrevious();
+};
+function getNext() {
+  index == sliderImages.length - 1 ? (index = 0) : index++;
+  sliderImg.src = sliderImages[index];
+}
+function getPrevious() {
+  index == 0 ? (index = sliderImages.length - 1) : index--;
+  sliderImg.src = sliderImages[index];
+}
